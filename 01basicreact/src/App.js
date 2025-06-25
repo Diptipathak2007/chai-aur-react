a2549190661caa631901e4aba0727eed8a278114
Diptipathak2007 <diptipathak019@gmail.com>
2025-06-25 17:45:51 +0530

    video no.2

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+  return (
+    <h1>chai aur react|this is me dipti pathak leanred here creating projects through react here used npxx </h1> 
+  );
+}
+
+export default App;
