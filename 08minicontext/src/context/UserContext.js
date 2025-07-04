37a3deb9555634ea39eb62c9a20e44ee8a258857
Diptipathak2007 <diptipathak019@gmail.com>
2025-07-04 23:50:38 +0530

    finally built minicontextapi project

--- 08minicontext/src/context/UserContext.js
+++ 08minicontext/src/context/UserContext.js
@@ -1,4 +1,4 @@
 import React from "react";
 const usercontext=React.createContext();
 
-export default usercontext;
+export default usercontext;//part-1 creating our context//basically we are creating a context object that we can use to share data across components without having to pass props down manually at every level.
